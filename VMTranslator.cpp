9dcbf49bc0ac0db1fede26c5eca46db5ef5c588c
UNAL Faruk <unalfaruk@outlook.com>
2021-01-09 20:58:02 +0300

    The structure/draft of Parser was created.

--- VMTranslator.cpp
+++ VMTranslator.cpp
@@ -1,11 +1,85 @@
 // VMTranslator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+using namespace std;
 #include <iostream>
+#include <fstream>
+#include <string>
+
+class Parser {
+public:
+    Parser(fstream &inputFile) {
+
+    }
+
+    bool hasMoreCommands() {
+
+    }
+
+    string advance() {
+
+    }
+
+    int commandType() {
+
+    }
+
+    string arg1() {
+
+    }
+
+    string arg2() {
+
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Welcome to VM Translator!" << endl;
+    cout << "\tby UNAL, Faruk\n" << endl;
+
+    //Open VM file first.
+    fstream vmFile;
+    string vmFile_fileName = "SimpleAdd.vm";
+    vmFile.open(vmFile_fileName);
+    if (!vmFile.is_open()) {
+        cout << "Error! VM file(" << vmFile_fileName << ") can not be opened!" << endl;
+        return 0;
+    }
+    //Clean the file, get rid of comments.
+    //Normally, this process is done by Parser component.
+    //I prefered this method.
+    cout << "VM file(" << vmFile_fileName << ") is being initialized...\n" << endl;
+    cout << "VM file will be re-organized...\n\tComments & empty lines will be removed...\n" << endl;
+
+    //Open a temporary file to save re-organized VM file in it.
+    string line;
+    int lineCounter = 0;
+
+    ofstream tmpVM;
+    tmpVM.open("~tmpVM.vm");
+    if (!tmpVM.is_open()) {
+        cout << "Error! Temporary VM file can not be created! Re-organizing failed!" << endl;
+        return 0;
+    }
+
+    while (getline(vmFile, line)) {
+        //Ignore comment lines and empty lines
+        if (line.substr(0, 2) == "//" || line == "")
+            continue;
+        //If there is a comment at end of the line, remove it
+        if (line.find("//") != string::npos)
+            line.replace(line.find("//"), line.length() - line.find("//"), "");
+
+        lineCounter++;
+
+        tmpVM << line << endl;
+    }
+    tmpVM.close();
+    vmFile.close();
+    cout << "Re-organized VM file(" << vmFile_fileName << ") created successfully!\n" << endl;
+
+    fstream inputFile;
+    inputFile.open("~tmpVM.vm");
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
